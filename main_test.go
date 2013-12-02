136c664471e3c2cac799d03c1f2290d45e0f3d77
Athiwat Chunlakhan <athiwatc@gmail.com>
2013-12-02 09:54:56 +0700

    Test

--- main_test.go
+++ main_test.go
@@ -4,14 +4,14 @@ import (
 	"testing"
 )
 
-func MainUnTest(t *testing.T) {
+func TestMainUn(t *testing.T) {
 	if aaa() != "asd" {
 		t.Log("pass1")
 	}
 }
 
-func MainEqTest(t *testing.T) {
-	if aaa() != "asd" {
+func TestMainEq(t *testing.T) {
+	if aaa() == "asd" {
 		t.Log("pass2")
 	}
 }
